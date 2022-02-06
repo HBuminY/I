6aabd14dd3d0264aec60879d5ab0430933b2e521
HBuminY <70579311+HBuminY@users.noreply.github.com>
2022-02-06 21:13:25 +0300

    done some stuff

---
+++ docs/index.js
@@ -0,0 +1,44 @@
+var i = 0;
+texts = [
+    "Web",
+    "Game",
+    "Desktop"
+];
+var txtIndex = 0;
+var txt = texts[txtIndex];
+var OriSpeed = 300;
+var speed = OriSpeed
+var elementId = "demo";
+
+function twWrite() {
+    if (i < txt.length) {
+        document.getElementById(elementId).innerHTML += txt.charAt(i);
+        i++;
+        if (speed > 100) {
+            speed -= 50
+        };
+        setTimeout(twWrite, speed);
+    } else if (i == txt.length) {
+        speed = OriSpeed;
+        console.log("removing");
+        setTimeout(twRemove, 2000)
+    };
+};
+
+function twRemove() {
+    if (i > -1) {
+        document.getElementById(elementId).innerHTML = txt.slice(0, i);
+        i--;
+        if (speed > 100) {
+            speed -= 50
+        };
+        setTimeout(twRemove, speed);
+    } else if (i == -1) {
+        speed = OriSpeed;
+        txtIndex++;
+        txt = texts[txtIndex % 3];
+        twWrite();
+    };
+};
+
+document.onload = twWrite();
