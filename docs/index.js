6c0b0e60295cf7cce213c1223bfe30933e694f0e
HBuminY <70579311+HBuminY@users.noreply.github.com>
2022-02-15 22:40:03 +0300

    fixed sumthn

--- docs/index.js
+++ docs/index.js
@@ -62,14 +62,17 @@ function loadPage(href) {
     return xmlhttp.responseText;
 };
 
-let activeBtn = document.getElementById("btn-0")
+let activeBtn = document.getElementById("btn-0");
+
 function goto(btn_no){
     let contDiv = document.getElementById('content');
     contDiv.innerHTML = loadPage("./contents/"+contents[btn_no]);
     //
     let btn = document.getElementById(`btn-${btn_no}`);
     btn.setAttribute("class","nav-link active");
+    btn.setAttribute("aria-current","page");
     activeBtn.setAttribute("class","nav-link");
+    btn.setAttribute("aria-current","false");
     activeBtn = btn;
 };
 
